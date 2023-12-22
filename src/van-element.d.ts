ffcc2bd8d43ca2f1871dbeeaac0fcdd4d74a2dd4
Atmos4 <atmos4.dev@gmail.com>
2023-12-22 13:49:49 +0100

    Release 0.1.4

--- src/van-element.d.ts
+++ src/van-element.d.ts
@@ -1,1 +1,12 @@
-export { define } from "../types/van-element";
+import { State, ChildDom } from "vanjs-core";
+
+export type ElementProps = Record<string, State<any>> & {
+  onMount: (mountFunction: () => (() => void) | void) => void;
+  element: HTMLElement;
+};
+
+export declare const define: (
+  name: string,
+  element: (props: ElementProps) => ChildDom,
+  options?: { observed?: string[]; disableShadow?: boolean }
+) => void;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts({ entryRoot: "src", exclude: ["src/showcase.ts"] })],
   build: {
     lib: {
       entry: "src/van-element.js",
       name: "vanE",
+      fileName: (format) =>
+        `van-element${format == "umd" ? ".umd" : ""}.min.js`,
     },
     rollupOptions: {
       external: ["vanjs-core"],
